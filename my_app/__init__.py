6d9ab5ad35270bac13aaa01055a8ff8a5ebffeaf
Григорий Кузеванов <grkuzevanov@gmail.com>
2024-02-14 17:45:38 +0500

    add flask moment

--- my_app/__init__.py
+++ my_app/__init__.py
@@ -7,6 +7,7 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
+from flask_moment import Moment
 
 from my_app.celery_init import celery_init_app
 
@@ -17,6 +18,8 @@ app.config.from_object(Config)
 db = SQLAlchemy(app)
 
 migrate = Migrate(app, db)
+
+moment = Moment(app)
 
 login = LoginManager(app)
 login.login_view = "login"
